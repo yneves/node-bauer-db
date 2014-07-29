d0ee212958d757dd261a5bedadc62c1766af6749
Yuri Neves <yurinevessilveira@hotmail.com>
2014-07-29 14:37:47 -0300

    0.1.0

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ node-bauer-db
 =============
 
 Modern API for SQL databases.
+
+Currently supports `sqlite3` database.
+
+## Installation
+
+```
+npm install bauer-db
+```
+
+## Usage
+
+```js
+var db = require("bauer-db");
+```
+
+## Documentation
+
+Pending.
+
+## License
+
+MIT
---
+++ db.js
@@ -0,0 +1,42 @@
+// - -------------------------------------------------------------------- - //
+// - exports
+
+var sql = require("./lib/sql.js");
+var Local = require("./lib/local.js");
+var Remote = require("./lib/remote.js");
+var Server = require("./lib/server.js");
+var Database = require("./lib/database.js");
+
+exports = {};
+
+exports.local = function() { return new Local() }
+exports.remote = function() { return new Remote() }
+exports.server = function() { return new Server() }
+exports.database = function() { return new Database() }
+
+exports.drop = function(db) { return new sql.Drop(db) }
+exports.alter = function(db) { return new sql.Alter(db) }
+exports.create = function(db) { return new sql.Create(db) }
+exports.update = function(db) { return new sql.Update(db) }
+exports.insert = function(db) { return new sql.Insert(db) }
+exports.delete = function(db) { return new sql.Delete(db) }
+exports.select = function(db) { return new sql.Select(db) }
+
+exports.cls = {};
+
+exports.cls.Local = Local;
+exports.cls.Remote = Remote;
+exports.cls.Server = Server;
+exports.cls.Database = Database;
+
+exports.cls.Drop = sql.Drop;
+exports.cls.Alter = sql.Alter;
+exports.cls.Create = sql.Create;
+exports.cls.Insert = sql.Insert;
+exports.cls.Update = sql.Update;
+exports.cls.Delete = sql.Delete;
+exports.cls.Select = sql.Select;
+
+module.exports = exports;
+
+// - -------------------------------------------------------------------- - //
